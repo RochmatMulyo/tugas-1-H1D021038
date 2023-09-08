0d8e47ba95748eff8df9a72ac6ef619026656b93
RochmatMulyo <127836890+RochmatMulyo@users.noreply.github.com>
2023-09-08 13:07:17 +0700

    Tugas Pertama

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 class Mahasiswa {
   String nim;
   String nama;
@@ -6,10 +8,12 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    int usia = DateTime.now().year-tahunLahir;
+    print("Perkenalkan, nama saya $nama, dengan NIM $nim dan usia saya $usia tahun.");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  Mahasiswa mhs = Mahasiswa("H1D021038", "Rochmat Mulyo Sugihono", 2004);
+  mhs.perkenalan();
 }
